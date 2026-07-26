ea30af427f1f685d5a2b4099f354747b9cb86cce
LaurencyLRC <kawayurilaura@gmail.com>
2026-07-27 00:36:41 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,11 +40,12 @@ export const metadata: Metadata = {
   ],
   authors: [{ name: "Laurency" }],
   icons: {
-  icon: [
-    { url: "/favicon-48x48.png", sizes: "32x32", type: "image/png" },
-    { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-  ],
+    icon: [
+      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+  },
 };
 
 export default function RootLayout({
